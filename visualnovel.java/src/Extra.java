47501ef92e83502b8ef54df79b665c9246a3c322
yourmoms-boyfriend <25kvang5883@apps-hcpak12.org>
2023-02-02 16:45:59 -0600

    Started on the visual novel

--- visualnovel.java/src/Extra.java
+++ visualnovel.java/src/Extra.java
@@ -4,54 +4,516 @@ public class Extra {
 
     public static void main(String[] args){
 
-            System.out.println("Yo wsp");
+    System.out.println("\033[3mAlright, hand over the food. I'm not going to ask again.\033[0m");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("Great. How did things end up this way?");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("You were just minding your own business, trying to gather supplies to survive out here.");
+    try {
+        Thread.sleep(4000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("Then suddenly...");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("\033[3mAlright kid, turn around and drop all the food you got with ya.\033[0m");
+    try {
+        Thread.sleep(3000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("A demanding voice shouted out of nowhere.");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("A group of bandits. Uncommon around these parts...");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("They don't ever look for their own supplies.");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("Always stealing from and threatening others.");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println("You sigh audibly, tired already from today.");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What kind of approach should you try?");
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }  
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("u wanna start the game bro?");
-        System.out.println("yes");
-        System.out.println("no");
-
-        String input = scanner.nextLine();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }  
-        
-        if (input.toLowerCase().equals("yes")){
-            System.out.println("Ok then lets start lol");
         }
 
-        if (input.toLowerCase().equals("no")){
+        System.out.println("[A] Friendly");
+        System.out.println("[B] Threaten");
 
-            System.out.println("...");
+        String input = scanner.nextLine( ); 
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(".....");
-
+        if (input.toLowerCase().equals("a")){
+            System.out.println("You decide that taking a friendly approach would be best.");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("let's start lol");
-        
+            System.out.println("After all, you were outnumbered 4 to 1.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            System.out.println("\033[3mI haven't eaten in a good long while..\033[0m");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[3mYou guys probably haven't either, so I can share some instead if you'd like.\033[0m");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("You really had no intention of sharing.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("You've got your own people to feed as well.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("These scummy bandits aren't getting any bit of food you've managed to scavenge.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[3mWe aren't playing around, kid. Hand over the food or your brains will get splattered all over these-\033[0m");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (input.toLowerCase().equals("b")){
+            System.out.println("You decide that playing nice will get you nowhere.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("These men were bandits.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Most likely, they've never felt empathy. Ever.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[3mMy buddy's around here.\033[0m");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[3mHe'll kill you all in a second. I suggest you get lost before that happens.\033[0m");
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("It wasn't a lie.");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("You really did have a 'buddy' around here that will show up any second now.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("But could he kill 4 men in a second? Probably not. Might take him about 3 seconds.");
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[3mSeriously?\033[0m");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("All 4 bandits bursted out into fits of laughter.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("It seems like they think you're bluffing.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        System.out.println("Their voice was cut off as a large figure stood over all of you.");
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        System.out.println("\033[3mOnce upon a time uhh.\033[0m");
-    }
-}
+        }
+
+        System.out.println("A hand bigger than the size of the bandit's head grabs his neck and pulls him back so fast it was like he disappeared...");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("He was thrown into the wall effortlessly by the figure.");
+        try {
+            Thread.sleep(2000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+         System.out.println("The rest of the men were slaughtered like pigs in a butcher's shop.");
+         try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mYou're finally here.\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Your buddy, or more like partner- was finally here.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mSorry.\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("His low voice echoed through the empty dishleveled room/");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mI got hungry.\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Something he often says.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("He did have a ravenous apetite after all. He'd even eat you if he was hungry enough.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mIt's fine.\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("You say calmly.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mI'm just glad you got here in time.\033[0m");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Around a few months ago, you had met this man.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Could he be called even a man?");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("It was like finding a hungry stray on the street. Except it was huge and monstrous.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Poor guy was dying of hunger. He nearly bit your arm off when you offered him food.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("His name was Thanatos. Though, you call him Hana for short.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hana yawned, his mouth opening widely and flaunting his long, sharp teeth.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Your arm jerked a bit, an instinct of yours.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Even after working and being around this guy for several months,");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Your body still tried to flee whenever he did something as simple as yawn.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("This room is now empty.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("You suppose it's time to go back to base before any more bandits show up.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3mAlright, let's go.\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("You say, letting Hana know he can start walking instead of drifting off in place repeatedly.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\033[3m...\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("So quiet...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Even if you two had been working together for a few months, you never knew Hana that well.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Food was scarce, and he seemed to just be tired because he was hungry all the time.");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("You glanced away from him as you both left the abandoned building.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("The silence was ridiculously loud.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Maybe it's time to talk to Hana.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Scanner scanner2 = new Scanner(System.in);
+
+        System.out.println("What do you do?");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("[A] Talk to Hana");
+        System.out.println("[B] Just Keep Walking");
+
+        String input2 = scanner.nextLine( ); 
+
+        if (input.toLowerCase().equals("a")){
+            System.out.println("You figured that you should probably talk to Hana.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        System.out.println("\033[3m...\033[0m");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("What do you even say to him?");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hana never spoke much.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        
+        }
 }}
